30be5112fd8b54574e75b7acdf369a8258e2d738
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2018-03-08 11:55:56 -0600

    Removed clang stuff from source-me

--- source-me.sh
+++ source-me.sh
@@ -2,18 +2,6 @@
 # our build directory root for all clang compiled projects in one place
 export BUILD_ROOT=$PWD
 mkdir -p clang6
-export CLANG_ROOT=$BUILD_ROOT/clang6
-export CC=$CLANG_ROOT/bin/clang
-export CXX=$CLANG_ROOT/bin/clang++
-export CPP=$CLANG_ROOT/bin/clang-cpp
-#
-export PATH=$CLANG_ROOT/bin:$PATH
-export LD_LIBRARY_PATH=$CLANG_ROOT/lib:$LD_LIBRARY_PATH
-#
-export CFLAGS=-fPIC
-export CXXFLAGS="-fPIC -march=native -mtune=native -ffast-math -std=c++14 -stdlib=libc++ -I$CLANG_ROOT/include/c++/v1"
-export LDFLAGS="-L$CLANG_ROOT/lib -rpath $CLANG_ROOT/lib"
-export LDCXXFLAGS="$LDFLAGS -std=c++14 -stdlib=libc++"
 #
 if [[ `echo $HOST | grep vgpu2` ]]; then
     echo "compiling for vgpu2, doing additional setup";
