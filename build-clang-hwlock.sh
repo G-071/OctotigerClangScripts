896d5feb7505cc1229ef312739bbc62faaefc3c7
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2018-03-08 20:35:57 +0100

    Removed opencl from hwloc

--- build-clang-hwlock.sh
+++ build-clang-hwlock.sh
@@ -10,5 +10,5 @@ cd $BUILD_ROOT
 wget --no-check-certificate https://www.open-mpi.org/software/hwloc/v1.11/downloads/hwloc-$HWLOC_VER.tar.gz
 tar -xzf hwloc-$HWLOC_VER.tar.gz
 cd hwloc-$HWLOC_VER
-./configure --prefix=$INSTALL_ROOT/hwloc/$HWLOC_VER
+./configure --prefix=$INSTALL_ROOT/hwloc/$HWLOC_VER --disable-opencl
 make -j8 install
